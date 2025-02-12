98a965c318b0c59b088fe962ff8a6096d7933231
luiz-cazarin <cazarin.junior@gmail.com>
2025-02-12 16:47:39 -0300

    Add account entity

---
+++ src/main/java/com/example/aggregadordeinvestimentos/entity/Account.java
@@ -0,0 +1,42 @@
+package com.example.aggregadordeinvestimentos.entity;
+
+import jakarta.persistence.*;
+import jdk.jfr.Description;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
+public class Account {
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID account_id;
+
+    @Column(name = "description")
+    private String description;
+
+    public Account(UUID account_id, String description) {
+        this.account_id = account_id;
+        this.description = description;
+    }
+
+    public Account() {
+    }
+
+    public UUID getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(UUID account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
